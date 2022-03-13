87fd6d327ee635692fc17634bdcd894c61700eb7
edin29 <100796896+edinsabic@users.noreply.github.com>
2022-03-13 21:47:12 +0100

    Created the board
    Started working on Four in a line. I created the board, that's it for now.

---
+++ Four in a line/FIL.c
@@ -0,0 +1,36 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+WINDOW* novoPolje(int h, int w, int sy, int sx) {
+	WINDOW* win = newwin(h, w, sy, sx); 
+	box(win, '|', '-');
+
+	return win;
+}
+
+int main() {
+	initscr();
+	curs_set(0);
+
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
+
+	WINDOW** polja = (WINDOW**) malloc(42 * sizeof(WINDOW*)); // free this!
+	
+	int h = yMax / 7, w = xMax / 7, sy = yMax/7, sx = xMax / 7;
+	
+	refresh();
+
+	for (int i = 0; i < 42; i++) {
+		polja[i] = novoPolje(h, w, yMax - (i / 7 + 1) * sy, (i % 7) * sx);
+		wrefresh(polja[i]);
+	}
+	
+	free(polja);
+
+	getch();
+	endwin();
+	return 0;
+}
