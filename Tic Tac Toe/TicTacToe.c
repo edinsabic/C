41a3621428ee85b90aeca5e30aa23383dc21b967
edinsabic <sabic.edin29@gmail.com>
2022-03-15 21:43:59 +0100

    Hotfix
    Popravil sem par stvari, ampak dejasnko sem commit in branch ustvaril, da vidim, če dobro razumem sistem GitHub-a

--- Tic Tac Toe/TicTacToe.c
+++ Tic Tac Toe/TicTacToe.c
@@ -5,6 +5,7 @@
 
 int counter = 0;
 int igreJeKonec = 0;
+char string[60] = "Enter a number betwen 1 and 9 to enter your simbol: ";
 
 WINDOW* novoOkno(int h, int w, int sy, int sx) {
 	WINDOW* win = newwin(h, w, sy, sx); 
@@ -18,17 +19,17 @@ void polje(int c, char zmagovalec, WINDOW** okna, int* memoizacijaPolja) {
 		if (memoizacijaPolja[c - 1] == 1 || memoizacijaPolja[c - 1] == 2) {
 			if (!igreJeKonec)
 				mvprintw(3, 0, "You've selected and input that's already been chosen, please try again!");
-			move(0, 52);
+			move(0, strlen(string));
 		} else {
 			mvwprintw(okna[c - 1], 3, 6, "%c", zmagovalec);	
 			memoizacijaPolja[c - 1] = (counter % 2 == 0) ? 1 : 2;
-			move(0, 52);
+			move(0, strlen(string));
 			counter++;
 		}
 	} else {
 		if (!igreJeKonec)
 			mvprintw(3, 0, "Your input is invalid. Please try again!");
-		move(0, 52);
+		move(0, strlen(string));
 	}
 }
 
@@ -39,7 +40,7 @@ void zmagovalnaKombinacija(int x, int y, int z, int* memoizacijaPolja) {
 			char winner = (counter % 2 == 0) ? 'O' : 'X';
 			mvprintw(5, 0, "Game over!\nPlayer %c has won the game!\nPress any key to exit.", winner);
 		}
-		move(0,52);
+		move(0, strlen(string));
 	}
 }
 
@@ -50,8 +51,8 @@ int main() {
 	int yMax, xMax;
 	getmaxyx(stdscr, yMax, xMax);
 
-	WINDOW** okna = (WINDOW**) malloc(9 * sizeof(WINDOW*)); // free this!
-	int* memoizacijaPolja = calloc(9, sizeof(int)); // free this!
+	WINDOW** okna = (WINDOW**) malloc(9 * sizeof(WINDOW*));
+	int* memoizacijaPolja = calloc(9, sizeof(int));
 
 	int h = ((yMax / 2) / 3), w = ((xMax / 2) / 3);
 
@@ -59,13 +60,13 @@ int main() {
 	okna[1] = novoOkno(h, w, (yMax / 4) + (0   /2/3) - 0, (xMax / 4) + (xMax/2/3));
 	okna[2] = novoOkno(h, w, (yMax / 4) + (0   /2/3) - 0, (xMax / 4) + (xMax/1/3));
 	okna[3] = novoOkno(h, w, (yMax / 4) + (yMax/2/3) - 0, (xMax / 4) + (0   /2/3));
-	okno[4] = novoOkno(h, w, (yMax / 4) + (yMax/2/3) - 0, (xMax / 4) + (xMax/2/3));
+	okna[4] = novoOkno(h, w, (yMax / 4) + (yMax/2/3) - 0, (xMax / 4) + (xMax/2/3));
 	okna[5] = novoOkno(h, w, (yMax / 4) + (yMax/2/3) - 0, (xMax / 4) + (xMax/1/3));
 	okna[6] = novoOkno(h, w, (yMax / 4) + (yMax/1/3) - 1, (xMax / 4) + (0   /2/3));
 	okna[7] = novoOkno(h, w, (yMax / 4) + (yMax/1/3) - 1, (xMax / 4) + (xMax/2/3));
 	okna[8] = novoOkno(h, w, (yMax / 4) + (yMax/1/3) - 1, (xMax / 4) + (xMax/1/3));
 
-	mvprintw(0, 0, "Enter a number betwen 1 and 9 to enter your simbol: ");
+	mvprintw(0, 0, "%s", string);
 	refresh();
 
 	while (1) {
@@ -95,8 +96,8 @@ int main() {
 		zmagovalnaKombinacija(2, 4, 6, memoizacijaPolja);
 
 		if (counter == 9) {
-			mvprintw(7, 0, "Game over! Noone won. Press any key to exit.");
-			move(0,52);
+			mvprintw(7, 0, "Game over! Nobody won. Press any key to exit.");
+			move(0, strlen(string));
 			igreJeKonec = 1;
 		}
 
