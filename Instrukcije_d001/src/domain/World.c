42373336401ba0efa74da47bf4151a53b9a98c56
edinsabic <sabic.edin29@gmail.com>
2022-03-27 05:44:27 +0200

    Popravljeni so vse stvari, ki so bile za popravit, torej vsi komentarji Uroša; ostalo je samo še dinamično preverjanje zmagovalca

--- Instrukcije_d001/src/domain/World.c
+++ Instrukcije_d001/src/domain/World.c
@@ -3,15 +3,44 @@
 //
 
 #include "World.h"
+#include <stdlib.h>
 
 World world_new() { // ker imamo fixno dolzino ne rabimo parametrov
     World this;
 
-    this.height = 3;
-    this.width = 3;
+    // Vedno pri prirejanju elementov struct dodaj type cast
+    this = (World){.height = 3, .width = 3, .active = 0, .frames = 0, .zmagovalneKombinacije = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{0,4,8},{2,4,6}}};
+
     for (int i = 0; i < WORLD_ST_ZETONOV; i++) {
         this.zetoni[i].aktiven = 0;
     }
 
     return this;
+}
+
+// Zakaj memoTabela mora bit const?
+int jeKdoZmagal(int tabela[ST_ZMAGOVALNIH_KOMBINACIJ][3], int memoTabela[WORLD_ST_ZETONOV], World world) {
+    for (int i = 0; i < ST_ZMAGOVALNIH_KOMBINACIJ; ++i) {
+        if ((memoTabela[tabela[i][0]] > 0 && memoTabela[tabela[i][1]] > 0 && memoTabela[tabela[i][2]] > 0)
+            && (memoTabela[tabela[i][0]] == memoTabela[tabela[i][1]])
+            && (memoTabela[tabela[i][0]] == memoTabela[tabela[i][2]])) {
+
+            // world.active = 1;
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void poteza(World* world, int* memoTabela, int c) {
+    int zaVnest = (world->frames % 2) ? 1 : 2;
+    memoTabela[c] = zaVnest;
+
+    char znak = (world->frames % 2) ? 'O' : 'X';
+
+    Zeton zeton = zeton_new(c % 3, c / 3, znak);
+    world->zetoni[world->frames] = zeton; // dodelimo tabeli zetonov zeton
+
+    world->frames++;
 }
--- Instrukcije_d001/src/domain/World.h
+++ Instrukcije_d001/src/domain/World.h
@@ -14,8 +14,14 @@ typedef struct World {
     int height;
     int width;
     Zeton zetoni[WORLD_ST_ZETONOV];
+    int active; // flag za to, ali je terminal živ ali mrtev
+    int frames; // counter za to kdo bo na vrsti (X ali 0) -> which frame we're at
+    int memoTabela[WORLD_ST_ZETONOV];
+    int zmagovalneKombinacije[ST_ZMAGOVALNIH_KOMBINACIJ][3];
 } World;
 
 World world_new();
+int jeKdoZmagal(int tabela[ST_ZMAGOVALNIH_KOMBINACIJ][3], int memoTabela[WORLD_ST_ZETONOV], World world);
+void poteza(World* world, int* memoTabela, int c);
 
 #endif //INSTRUKCIJE_D001_WORLD_H
