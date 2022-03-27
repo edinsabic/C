388e014784fe8dbfd5247340800fd15855cb3782
edinsabic <sabic.edin29@gmail.com>
2022-03-27 15:12:48 +0200

    Cleaner main

---
+++ Instrukcije_d001/cmake-build-debug/src/CMakeFiles/Instrukcije_d001.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/src/app/terminal.c" "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/cmake-build-debug/src/CMakeFiles/Instrukcije_d001.dir/app/terminal.c.o"
+  "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/src/domain/World.c" "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/cmake-build-debug/src/CMakeFiles/Instrukcije_d001.dir/domain/World.c.o"
+  "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/src/domain/Zeton.c" "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/cmake-build-debug/src/CMakeFiles/Instrukcije_d001.dir/domain/Zeton.c.o"
+  "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/src/main.c" "/mnt/c/Users/Edin/Desktop/C/GitHub/Instrukcije_d001/cmake-build-debug/src/CMakeFiles/Instrukcije_d001.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/app"
+  "../src/domain"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
